71a80cbdd63ee78b8d3aba3bd6accce050ff2e12
Sai Avinash <saiavinashvenkata@gmail.com>
2025-08-24 18:57:06 -0500

    added table view

---
+++ HackNews/SampleViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SampleViewController.swift
+//  HackNews
+//
+//  Created by Sai Avinash Vagicherla on 8/23/25.
+//
+
+import UIKit
+
+class SampleViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    override func loadView() {
+    
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+        @IBAction func buttonAction(_ sender: Any) {
+            if let userName = userNameTextField.text, userName.count > 6 {
+                view.backgroundColor = .systemGreen
+            } else {
+                view.backgroundColor = .systemRed
+            }
+        }
+
+    
+//    @IBAction func buttonAction(_ sender: Any) {
+//        if let userName = userNameTextField.text, userName.count > 6 {
+//            view.backgroundColor = .systemGreen
+//        } else {
+//            view.backgroundColor = .systemRed
+//        }
+//    }
+
+}
